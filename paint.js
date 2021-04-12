bd3142f8a4039ae817b91dc216eec7458b4ae91b
Zasaimster <Zasaimster@gmail.com>
2021-04-12 16:09:52 -0700

    added auto updating text that displays hex and rgb values of the current color

--- paint.js
+++ paint.js
@@ -2,8 +2,8 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 const colors = {
-	default: '#000',
-	background: '#fff',
+	default: '#000000',
+	background: '#ffffff',
 	red: '#fc0303',
 	green: '#00ff4c',
 	blue: '#008cff',
@@ -33,6 +33,38 @@ let offsetY = canvas.offsetHeight;
 
 var pointsData = [];
 
+const getNumberForHexCharacter = (val) => {
+	switch (val) {
+		case !isNaN(parseInt(val)):
+			return parseInt(val);
+		case 'a':
+			return 10;
+		case 'b':
+			return 11;
+		case 'c':
+			return 12;
+		case 'd':
+			return 13;
+		case 'e':
+			return 14;
+		case 'f':
+			return 15;
+		default:
+			return 0;
+	}
+}
+
+const convertColorToRGB = (color) => {
+	let rgb = [];
+	for(let i = 0, colorIndex = 1; i < 3; i++, colorIndex += 2) {
+		rgb[i] = 16 * getNumberForHexCharacter(color.charAt(colorIndex)) + getNumberForHexCharacter(color.charAt(colorIndex + 1));
+	}
+	console.log('Current RGB colors:', rgb[0], rgb[1], rgb[2])
+
+
+	return rgb[0].toString() + " " + rgb[1].toString() + " " + rgb[2].toString();
+}
+
 document.querySelectorAll('.color-selector').forEach((colorSelector) => {
 	console.log('in query selcetor');
 	colorSelector.addEventListener('click', () => {
@@ -53,6 +85,8 @@ document.querySelectorAll('.color-selector').forEach((colorSelector) => {
 			default:
 				break;
 		}
+		document.getElementById('rgb-color-value').innerHTML =  'RGB: ' + convertColorToRGB(selectedColor);
+		document.getElementById('hex-color-value').innerHTML =  'Hexadecimal: ' + selectedColor;
 	});
 });
 
